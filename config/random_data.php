eee2f1013823297fd34438bd9c088ee06c578963
MariaBuslaeva <maria.singlefox@gmail.com>
2019-08-13 21:07:59 +0300

    lesson16

---
+++ config/random_data.php
@@ -0,0 +1,33 @@
+<?php  
+
+return [
+
+	'car_brands' => [
+		'BMW',
+		'Lada',
+		'Audi',
+		'Aston Martin',
+		'Jaguar',
+		'Opel',
+		'Mazda',
+		'Ford',
+		'Honda',
+		'Toyota',
+	],
+
+	'car_models' => [
+		'520d',
+		'DB9',
+		'A6',
+		'Polo',
+		'Astra',
+		'XF',
+		'Focus',
+		'C-HR',
+		'Supra',
+	]
+
+];
+
+
+?>
